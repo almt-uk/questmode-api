43b5d6bd146bbd6a9e7942a7e0239bb2baadfd0b
Teodor G <grigor.teodor@gmail.com>
2020-03-08 18:10:24 +0000

    Update test_web.php

--- test_web.php
+++ test_web.php
@@ -5,4 +5,6 @@ require_once $path . '/db_handler/web.php';
 
 $db = new DbHandlerWeb();
 
+$db->checkApi("0RMs4Z0pFV7TNk9hsctm7w0ZdV2QWm9yuOawQJZm", "0RMs4Z0pFV7TNk9hsctm7w0ZdV2QWm9yuOawQJZm");
+
 ?>
